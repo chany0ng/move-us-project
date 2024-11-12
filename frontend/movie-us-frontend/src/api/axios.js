681d6c3bc326853bc6e607a0ec960ed0aacd6666
chany0ng <krap5075@naver.com>
2024-11-12 17:48:37 +0900

    Fix: axios 수정

--- frontend/movie-us-frontend/src/api/axios.js
+++ frontend/movie-us-frontend/src/api/axios.js
@@ -8,57 +8,53 @@ export const customAxios = axios.create({
   // withCredentials: true,
 });
 
-// 요청 interceptor
-// customAxios.interceptors.request.use(
-//   (config) => {
-//     const accessToken = localStorage.getItem("accessToken");
-//     if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
-//     if (config.data instanceof FormData) {
-//       config.headers["Content-Type"] = "multipart/form-data";
-//     } else {
-//       config.headers["Content-Type"] = "application/json";
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+//요청 interceptor
+customAxios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    if (config.data instanceof FormData) {
+      config.headers["Content-Type"] = "multipart/form-data";
+    } else {
+      config.headers["Content-Type"] = "application/json";
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-// // 응답 interceptor
-// customAxios.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const errorStatus = error.response?.status;
-//     console.log(error);
-//     // accessToken 만료 시 받는 status를 정해야함.
-//     if (error.response && errorStatus === 401) {
-//       alert("로그인 세션이 만료되었습니다. 다시 로그인해주세요.");
+// 응답 interceptor
+customAxios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const errorStatus = error.response?.status;
+    console.log(error);
+    // accessToken 만료 시 받는 status를 정해야함.
+    if (error.response && errorStatus === 401) {
+      alert("로그인 세션이 만료되었습니다. 다시 로그인해주세요.");
 
-//       // 토큰을 제거하고 로그인 페이지로 이동
-//       localStorage.removeItem("accessToken");
-//       window.location.href = "/"; // 로그인 페이지로 리다이렉트
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+      // 토큰을 제거하고 로그인 페이지로 이동
+      localStorage.removeItem("accessToken");
+      window.location.href = "/"; // 로그인 페이지로 리다이렉트
+    }
+    return Promise.reject(error);
+  }
+);
 
 // Access Token 재발급 함수
-// const refreshAccessToken = async () => {
-//   try {
-//     const response = await postData("/api/reissue", "");
-//     const newAccessToken = response.headers?.authorization; // 새로운 액세스 토큰 가져오기
+const refreshAccessToken = async () => {
+    const response = await postData("/api/reissue", "");
+    const newAccessToken = response.headers?.authorization; // 새로운 액세스 토큰 가져오기
 
-//     if (newAccessToken) {
-//       localStorage.setItem("accessToken", newAccessToken); // 로컬 스토리지에 저장
-//       return newAccessToken;
-//     } else {
-//       throw new Error("새로운 토큰을 가져올 수 없습니다.");
-//     }
-//   } catch (error) {
-//     throw error; // 인터셉터에서 예외 처리
-//   }
-// };
+    if (newAccessToken) {
+      localStorage.setItem("accessToken", newAccessToken); // 로컬 스토리지에 저장
+      return newAccessToken;
+    } else {
+      throw new Error("새로운 토큰을 가져올 수 없습니다.");
+    }
+};
 
 // User이름 -> UserNum 받아오기
 export const getUserNum = async (url, userName) => {
